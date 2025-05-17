4529f646f380e9317ee4c023442171981dd065f7
baer1 <videocreator@outlook.de>
2025-05-17 22:22:52 +0200

    Fix mod, improve functionality, disable as client (host-only)

--- MansionRend.cs
+++ MansionRend.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using BepInEx;
 using BepInEx.Logging;
@@ -9,89 +10,86 @@ namespace MansionRend;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class MansionRend : BaseUnityPlugin
 {
-    public static MansionRend Instance { get; private set; } = null!;
+    internal const int REND = 6;
+    internal const int MANSION = 1;
+
     internal static new ManualLogSource Logger { get; private set; } = null!;
     internal static Harmony? Harmony { get; set; }
 
     private void Awake()
     {
         Logger = base.Logger;
-        Instance = this;
 
-        Patch();
+        Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+        Logger.LogDebug("Patching...");
+        Harmony.PatchAll();
+        Logger.LogDebug("Finished patching!");
 
         Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
     }
 
-    [HarmonyPatch(typeof(RoundManager), nameof(RoundManager.GenerateNewFloor))]
-    public class GenerateNewFloorPatch
+    [HarmonyPatch(typeof(RoundManager), nameof(RoundManager.LoadNewLevelWait))]
+    public class LoadNewLevelWaitPatch
     {
-        private static void Prefix(ref RoundManager __instance)
+        // ReSharper disable once UnusedMember.Local
+        private static void Prefix(ref RoundManager __instance, ref int randomSeed)
         {
             Logger.LogDebug(
-                $">> GenerateNewFloor: {__instance.currentLevel.PlanetName} (id:{__instance.currentLevel.levelID}) {__instance.currentLevel.dungeonFlowTypes.Length} interiors found:"
+                $">> LoadNewLevelWait: {__instance.currentLevel.PlanetName} (seed:{randomSeed}, id:{__instance.currentLevel.levelID}) {__instance.currentLevel.dungeonFlowTypes.Length} interiors found:"
             );
+
             foreach (IntWithRarity i in __instance.currentLevel.dungeonFlowTypes)
                 Logger.LogDebug($"   {i.id} - {i.rarity}");
 
-            if (__instance.currentLevel.levelID != 6) // Rend
+            if (__instance.currentLevel.levelID != REND)
                 return;
 
-            Logger.LogDebug("Overwriting indoor generation chances...");
+            Logger.LogDebug("Finding seed with mansion generation...");
             try
             {
-                LevelAmbienceLibrary overrideLevelAmbience = (
-                    __instance.currentLevel.dungeonFlowTypes.Where((i) => i.id == 1)
-                )
-                    .First()
-                    .overrideLevelAmbience;
-                __instance.currentLevel.dungeonFlowTypes =
-                [
-                    new IntWithRarity()
-                    {
-                        id = 1,
-                        rarity = 300,
-                        overrideLevelAmbience = overrideLevelAmbience,
-                    },
-                ];
+                while (!IsGeneration(ref __instance, randomSeed, MANSION))
+                {
+                    Logger.LogDebug(
+                        $"Seed {randomSeed} does not have mansion generation, trying next seed"
+                    );
+                    StartOfRound.Instance.ChooseNewRandomMapSeed();
+                    randomSeed = StartOfRound.Instance.randomMapSeed;
+                }
+                Logger.LogDebug($"Seed {randomSeed} has mansion generation, proceeding");
             }
             catch (Exception e)
             {
                 Logger.LogError($"An unexpected error occurred: {e.GetType().Name} {e.StackTrace}");
-                Logger.LogWarning(
-                    "Skipping overrideLevelAmbience, this may cause errors in other parts of the game code."
-                );
-                __instance.currentLevel.dungeonFlowTypes =
-                [
-                    new IntWithRarity() { id = 1, rarity = 300 },
-                ];
             }
 
-            Logger.LogDebug(
-                $"<< GenerateNewFloor: {__instance.currentLevel.PlanetName} (id:{__instance.currentLevel.levelID}) {__instance.currentLevel.dungeonFlowTypes.Length} interiors found:"
-            );
-            foreach (IntWithRarity i in __instance.currentLevel.dungeonFlowTypes)
-                Logger.LogDebug($"   {i.id} - {i.rarity}");
+            if (IsGeneration(ref __instance, randomSeed, MANSION))
+                Logger.LogDebug($"<< LoadNewLevelWait: seed {randomSeed} has mansion generation");
+            else
+                Logger.LogError(
+                    $"<< LoadNewLevelWait: seed {randomSeed} does not have mansion generation"
+                );
         }
-    }
 
-    internal static void Patch()
-    {
-        Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+        internal static bool IsGeneration(
+            ref RoundManager __instance,
+            int randomSeed,
+            int generationId
+        )
+        {
+            if (
+                __instance.currentLevel.dungeonFlowTypes == null
+                || __instance.currentLevel.dungeonFlowTypes.Length == 0
+            )
+                return false;
 
-        Logger.LogDebug("Patching...");
+            List<int> list = [];
+            list.AddRange(__instance.currentLevel.dungeonFlowTypes.Select(i => i.rarity));
 
-        Harmony.PatchAll();
-
-        Logger.LogDebug("Finished patching!");
-    }
-
-    internal static void Unpatch()
-    {
-        Logger.LogDebug("Unpatching...");
-
-        Harmony?.UnpatchSelf();
-
-        Logger.LogDebug("Finished unpatching!");
+            int randomWeightedIndex = __instance.GetRandomWeightedIndex(
+                list.ToArray(),
+                new Random(randomSeed)
+            );
+            return __instance.currentLevel.dungeonFlowTypes[randomWeightedIndex].id == generationId;
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Simple mod that forces Mansion generation on 85-Rend
 
+Now fixed, only activates as host, doesn't put you out of bounds as client
+
 This mod might break in future versions of Lethal Company (tested on v69)
